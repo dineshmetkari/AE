601f58dd95295f2fdd7ec519805f2ad2a4a6b31e
ibigrock-ashish <ashishgupta@ibigrock.com>
2017-11-17 16:01:51 +0530

    Added Docker File

--- AdminService/src/test/java/com/example/Admin_demo/AdminDemoApplicationTests.java
+++ AdminService/src/test/java/com/example/Admin_demo/AdminDemoApplicationTests.java
@@ -1,16 +1,16 @@
-package com.example.Amdin_demo;
-
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
-
-@RunWith(SpringRunner.class)
-@SpringBootTest
-public class AdminDemoApplicationTests {
-
-	@Test
-	public void contextLoads() {
-	}
-
-}
+// package com.example.Amdin_demo;
+//
+// import org.junit.Test;
+// import org.junit.runner.RunWith;
+// import org.springframework.boot.test.context.SpringBootTest;
+// import org.springframework.test.context.junit4.SpringRunner;
+//
+// @RunWith(SpringRunner.class)
+// @SpringBootTest
+// public class AdminDemoApplicationTests {
+//
+// 	@Test
+// 	public void contextLoads() {
+// 	}
+//
+// }
