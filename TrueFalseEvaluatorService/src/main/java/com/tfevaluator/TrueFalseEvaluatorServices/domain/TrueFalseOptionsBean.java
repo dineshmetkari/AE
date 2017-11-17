7ded59533e091df015f3a4a985508afd2da23576
chioerry <charanksai@gmail.com>
2017-11-17 13:20:55 +0530

    Adding 2 seperate services for MCQ and TF

---
+++ TrueFalseEvaluatorService/src/main/java/com/tfevaluator/TrueFalseEvaluatorServices/domain/TrueFalseOptionsBean.java
@@ -0,0 +1,43 @@
+//package com.tfevaluator.TrueFalseEvaluatorServices.domain;
+//
+//
+//
+//public class TrueFalseOptionsBean {
+//	
+//	private String a;
+//	private String b;
+//	
+//	
+//	public TrueFalseOptionsBean() {
+//		super();
+//	}
+//
+//	public TrueFalseOptionsBean(String a, String b) {
+//		super();
+//		this.a = a;
+//		this.b = b;
+//	}
+//
+//	public String getA() {
+//		return a;
+//	}
+//
+//	public void setA(String a) {
+//		this.a = a;
+//	}
+//
+//	public String getB() {
+//		return b;
+//	}
+//
+//	public void setB(String b) {
+//		this.b = b;
+//	}
+//
+//	@Override
+//	public String toString() {
+//		return "a:" + a + ", b:" + b;
+//	}
+//	
+//	
+//}
